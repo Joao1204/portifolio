6c74c3b379fc63bfcf4493e7b5c964431273d920
Kirche <joao.kirchee02@gmail.com>
2025-07-12 16:10:07 -0300

    Hot Feat: configura main-title, main-header e footer.

--- src/components/mainFooter/MainFooter.js
+++ src/components/mainFooter/MainFooter.js
@@ -11,21 +11,26 @@ import './MainFooter.css';
 
 export default function MainFooter() {
 
-    return (
-        <div>
-        <div className='footer-description'>
-          <p>Site desenvolvido pelo João kirche.</p>
-          <p>Redes sociais abaixo, apenas clique no Icon <span>✓</span></p>
-          <div className='footer-copyright'>
-            <div className='footer-icon-email--container'>
-              <i className='footer-icon--email'><MdOutlineAttachEmail/></i>
-              <h3>E-mail: <a href="mailto:joao.kirchee02@gmail.com.br">joao.kirchee02@gmail.com.br</a></h3>
-            </div>
-         
+  return (
+      <div className='footer-wrapper'>
+        <div className='footer-center'>
+          <div className='footer-description'>
+              <p>Site desenvolvido pelo João kirche.</p>
+              <p>Redes sociais ao lado, apenas clique no Icon <span>✓</span></p><br/>
+              <h3>Copyright ©2025-present.</h3>
+              <h3>by: João Kirche.</h3>
           </div>
-          
         </div>
-        <div className='footer-icons'>
+
+        <div className='footer-right'>
+          <div className='footer-icon-email--container'>
+            <i className='footer-icon--email'><MdOutlineAttachEmail /></i>
+      
+            <h3>E-mail: <a href="mailto:joao.kirchee02@gmail.com.br">joao.kirchee02@gmail.com.br</a></h3>
+          </div>
+        </div>
+          
+        <div className='footer-left'>
           <div className='footer-icons--click'>
             <a href="https://github.com/Joao1204" target="_blank"><AiFillGithub /></a>
             <a href="https://www.linkedin.com/in/jo%C3%A3o-kirche-63b748267/" target="_blank"><AiFillLinkedin /></a>
@@ -34,11 +39,6 @@ export default function MainFooter() {
             <a href="https://api.whatsapp.com/send/?phone=%2B5511957768671&text&type=phone_number&app_absent=0" target="_blank"><IoLogoWhatsapp /></a>
           </div>
         </div>
-        <div className='footer-copyright'>
-            <h3>Copyright ©2025-present.</h3>
-            <h3>by: João Kirche.</h3>
-            
-        </div>
       </div>
-    )
+  )
 };
